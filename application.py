08e0af763251203ec3ba1dd180b12cdebe0eabc4
Alexander Sibagatov <43387913+sibalex@users.noreply.github.com>
2020-10-14 13:37:06 +0300

    Create application.py

---
+++ application.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+@application.route("/")
+def root():
+    return render_template("index.html")
+
+@application.route("/help")
+def helppage():
+    return render_template("help.html")
+
+@application.route("/hello")
+def index():
+    return "Hello World from Flask Hello Page.<b> v1.0"
+
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+    
